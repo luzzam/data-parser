daf1a80619bb20ecb501c7c18658b3c8a1292579
luzzam <261922493+luzzam@users.noreply.github.com>
2026-03-12 00:39:28 +1000

    Clean up dead code

--- parser.js
+++ parser.js
@@ -12,3 +12,13 @@ const calculateDelay = (retryCount) => {
     return Math.pow(2, retryCount) * 1000;
 };
 
+
+export const generateRandomToken = (length = 32) => {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let token = '';
+  for (let i = 0; i < length; i++) {
+    token += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return token;
+};
+
